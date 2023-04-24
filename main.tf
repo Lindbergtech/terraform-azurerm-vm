f206b34be8a18c380298cbd26cfb308ea6618ee0
Carl Lindberg <carl.lindberg@iver.se>
2023-04-24 11:24:52 +0200

    added vnet, subnet and NIC resource definitions

--- main.tf
+++ main.tf
@@ -4,3 +4,31 @@ resource "azurerm_resource_group" "this" {
 
   tags = local.tags
 }
+
+resource "azurerm_virtual_network" "this" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_prefix]
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+
+  tags = local.tags
+}
+
+resource "azurerm_subnet" "this" {
+  name                 = var.subnet_name
+  address_prefixes     = [var.subnet_prefix]
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+}
+
+resource "azurerm_network_interface" "this" {
+  name                = "${var.rg_name}-nic"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.this.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}

d2924f72195c2c8260bdd1aa3dd72571cf1e763d
Carl Lindberg <carl.lindberg@iver.se>
2023-04-24 11:25:05 +0200

    added variables for vnet and subnet

--- variables.tf
+++ variables.tf
@@ -9,3 +9,27 @@ variable "location" {
   type        = string
   default     = "West Europe"
 }
+
+variable "vnet_name" {
+  description = "The name of the Virtual Network"
+  type        = string
+  default     = "vnet-vm-t-weu-001"
+}
+
+variable "vnet_prefix" {
+  description = "The address space of the Virtual Network"
+  type        = string
+  default     = "10.0.0.0/22"
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+  default     = "default"
+}
+
+variable "subnet_prefix" {
+  description = "The prefix of the subnet inside the Virtual Network"
+  type        = string
+  default     = "10.0.0.0/24"
+}
